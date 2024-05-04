c53f8dc72c8ac51ece3be15cccb9c2394cd06fa5
FidelBM <133674887+FidelBM@users.noreply.github.com>
2024-05-04 13:36:08 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,15 +1,32 @@
-import express from 'express'
-import userRoutes from './routes/users.routes.js'
+// Importa Express para crear y configurar el servidor web
+import express from 'express';
+
+// Importa las rutas definidas para las operaciones CRUD de los usuarios
+import userRoutes from './routes/users.routes.js';
+
+// Importa el middleware para habilitar el intercambio de recursos entre servidores
 import cors from "cors";
+
+// Importa el middleware para registrar las solicitudes HTTP en la consola
 import morgan from "morgan";
 
+// Crea una instancia de la aplicación Express
 const app = express();
 
+// Habilita el middleware para permitir el intercambio de recursos entre servidores
 app.use(cors());
+
+// Habilita el middleware para registrar las solicitudes HTTP en la consola en formato "dev"
 app.use(morgan("dev"));
+
+// Habilita el middleware para analizar las solicitudes entrantes con datos codificados en URL
 app.use(express.urlencoded({ extended: false }));
+
+// Habilita el middleware para analizar las solicitudes entrantes con datos en formato JSON
 app.use(express.json());
 
-app.use(userRoutes)
+// Utiliza las rutas definidas en userRoutes para gestionar las solicitudes relacionadas con los usuarios
+app.use(userRoutes);
 
-export default app 
+// Exporta la aplicación Express configurada
+export default app;

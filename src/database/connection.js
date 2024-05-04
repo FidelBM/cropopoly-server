40db45e91db9efad8d739043325d510fa6b58daa
FidelBM <133674887+FidelBM@users.noreply.github.com>
2024-05-04 13:34:37 -0600

    Update connection.js

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,18 +1,21 @@
+// Importa la función createPool del paquete mysql2/promise
 import { createPool } from 'mysql2/promise';
 
+// Importa las variables de entorno que contienen la información de conexión desde el archivo de configuración config.js
 import {
     DB_HOST,
     DB_NAME,
     DB_PASSWORD,
     DB_USER,
     DB_PORT
-  } from '../config.js';
-  
+} from '../config.js';
 
+// Crea un pool de conexiones a la base de datos utilizando la función createPool
 export const pool = createPool({
-    user: DB_USER,
-    password:DB_PASSWORD,
-    host: DB_HOST,
-    port: DB_PORT,
-    database: DB_NAME
+    // Especifica los detalles de la conexión
+    user: DB_USER,          // Nombre de usuario de la base de datos
+    password: DB_PASSWORD,  // Contraseña de la base de datos
+    host: DB_HOST,          // Host de la base de datos
+    port: DB_PORT,          // Puerto de la base de datos
+    database: DB_NAME       // Nombre de la base de datos
 });
